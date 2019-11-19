64da439233cd5f45e27cb9f39cdb21e055fda152
Alberthoven2 <alb.1980@hotmail.com>
2019-11-19 20:48:06 +0100

    Introduzco el primer panel

--- src/proyectobingo/Bingo.java
+++ src/proyectobingo/Bingo.java
@@ -5,6 +5,9 @@
  */
 package proyectobingo;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author alb19
@@ -14,6 +17,18 @@ public class Bingo {
     private boolean hayBingo;
     private boolean hayLinea;
     private boolean seEstaJugando;
+    protected ArrayList <Bola> bolas = new ArrayList<>();
+    protected final Random rd;
+    
+    public Bingo(){
+        this.rd = new Random();
+        Bola b = new Bola();
+        for(int i = 0; i < 100; i++){
+            b.setNum(i + 1);
+            b.setDisponible(true);
+            bolas.add(b);
+        }
+    }
     
     protected void comienzaBingo(){
         this.seEstaJugando = true;
@@ -22,5 +37,33 @@ public class Bingo {
     protected void terminaBingo(){
         this.seEstaJugando = false;
     }
+    
+    protected void setBingo(boolean b){
+        this.hayBingo = b;
+    }
+    
+    protected void setLinea(boolean b){
+        this.hayLinea = b;
+    }
+    
+    protected int sacaBola(){
+        int bola;
+        boolean encontrada;
+        do{
+            encontrada = false;
+            bola = this.rd.nextInt(101);
+            for(int i = 0; i < bolas.size(); i++){
+                if(bolas.get(i).getNum() == bola){
+                    encontrada = true;
+                    bolas.get(i).setDisponible(false);
+                    bolas.remove(i);
+                    break;
+                }
+            }
+        }while(!encontrada);
+        return bola;
+    }
+    
+    
     
 }
